4cdc84d39b2c5796c85ddc37082a9f352a026fcb
Leonid Semenov <kalesin90@gmail.com>
2018-10-26 00:32:07 +0300

    Fix nonce

--- src/client/services/ethHelper.ts
+++ src/client/services/ethHelper.ts
@@ -5,7 +5,7 @@ const web3 = new Web3();
 web3.setProvider(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws'))
 
 export  async function getNonce (address: string): Promise<number> {
-  return  await web3.eth.getTransactionCount(address) + 1;
+  return  await web3.eth.getTransactionCount(address);
 }
 
 export function sendTx(tx) {
