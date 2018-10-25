e08c674bbed91a5543db6e025dc9bac15a79cfc1
Leonid Semenov <kalesin90@gmail.com>
2018-10-25 23:18:05 +0300

    Change wwebsocketprovider for rinkeby

--- src/client/services/ethHelper.ts
+++ src/client/services/ethHelper.ts
@@ -2,7 +2,7 @@ import Web3 = require('web3');
 
 const web3 = new Web3();
 
-web3.setProvider(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/OlWCtLVFGaNOXOgpelpw'))
+web3.setProvider(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws'))
 
 export  async function getNonce (address: string): Promise<number> {
   return  await web3.eth.getTransactionCount(address) + 1;
