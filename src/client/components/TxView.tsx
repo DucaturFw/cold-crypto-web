04a0eabd70e6cfca08e7c49fae78ea0ca35e455a
MaxSvargal <maxsvargal@gmail.com>
2018-10-26 23:17:04 +0700

    Restyle tx page

--- src/client/components/TxView.tsx
+++ src/client/components/TxView.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import {Column} from '../components/shared/layout'
+import React from 'react'
+import { Column, Container, Centered, Header } from '../components/shared/layout'
 import { H1 } from './shared/typography'
 import { connect} from 'react-redux'
 
 const TxView = ({error, lastTransaction : tx}) => {
   return (
-    <Column style={{ width: '45%', marginLeft: '5%' }}>
-      <H1>Tx sent result</H1>
-      {error
-        ? (<p>{error}</p>)
-        : (
-          <React.Fragment>
-            {`From : ${tx.from} To: ${tx.to}, txhash: ${tx.transactionHash}`}
-            <a target="_blank" href={`https://rinkeby.etherscan.io/tx/${tx.transactionHash}`}>
-              {`https://rinkeby.etherscan.io/tx/${tx.transactionHash}`}
-            </a>
-          </React.Fragment>
-        )
-      }   
-    </Column>
+    <>
+      <Header to='/' />
+      <Container>
+        <Centered>
+          <Column style={{ width: '45%', marginLeft: '5%' }}>
+            <H1>Tx sent result</H1>
+            {error
+              ? (<p>{error}</p>)
+              : (
+                <React.Fragment>
+                  {`From : ${tx.from} To: ${tx.to}, txhash: ${tx.transactionHash}`}
+                  <a target="_blank" href={`https://rinkeby.etherscan.io/tx/${tx.transactionHash}`}>
+                    {`https://rinkeby.etherscan.io/tx/${tx.transactionHash}`}
+                  </a>
+                </React.Fragment>
+              )
+            }
+          </Column>
+        </Centered>
+      </Container>
+    </>
   )
 };
 
