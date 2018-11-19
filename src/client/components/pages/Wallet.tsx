c9881eb723fca239f8f37833bfa756631e1ff6af
zlumer <zlumer@gmail.com>
2018-11-19 19:06:10 +0300

    capitalization

--- src/client/components/pages/Wallet.tsx
+++ src/client/components/pages/Wallet.tsx
@@ -11,7 +11,7 @@ import ButtonBase from '../atoms/ButtonBase'
 import H1 from '../atoms/H1'
 import H2 from '../atoms/H2'
 import Hr from '../atoms/Hr'
-import Column from '../atoms/column'
+import Column from '../atoms/Column'
 import Row from '../atoms/Row'
 
 interface IProps {
