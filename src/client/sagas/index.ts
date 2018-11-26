a1de6d3741186dbd5e41e61274b643c3454cc961
MaxSvargal <maxsvargal@gmail.com>
2018-11-26 23:44:38 +0700

    Enable remote sign saga

--- src/client/sagas/index.ts
+++ src/client/sagas/index.ts
@@ -1,14 +1,17 @@
 import { all, fork } from 'redux-saga/effects'
 
 import blockchainsSaga from './blockchainsSaga'
+import remoteSignSaga from './remoteSignSaga'
 import routerSaga from './routerSaga'
 import walletsSaga from './walletsSaga'
 import webRtcSaga from './webrtcSaga'
 
 export default function* rootSaga() {
+  yield fork(routerSaga)
+
   yield all([
-    fork(routerSaga),
     fork(blockchainsSaga),
+    fork(remoteSignSaga),
     fork(walletsSaga),
     fork(webRtcSaga),
   ])
