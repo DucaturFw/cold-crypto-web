4ef3ecf38c162af07df87eb1119a34e24e105952
MaxSvargal <maxsvargal@gmail.com>
2018-11-26 23:54:37 +0700

    Enable debug, disable IPFS

--- src/client/sagas/remoteSignSaga.ts
+++ src/client/sagas/remoteSignSaga.ts
@@ -2,7 +2,7 @@ import { call, put, take, fork } from 'redux-saga/effects'
 import { eventChannel, delay } from 'redux-saga'
 
 import { WebRTC } from '../services/webrtc'
-import { uploadFile, downloadFile } from '../helpers/ipfs'
+// import { uploadFile, downloadFile } from '../helpers/ipfs'
 
 import {
   setWebRtcConnectionState,
@@ -14,24 +14,26 @@ import {
 export default function* remoteSignSaga() {
   const rtc = new WebRTC()
   const offer = yield call(rtc.createOffer)
+  console.log({ offer })
   yield delay(5000)
   const candidates = rtc.candidates
+  console.log({ candidates })
 
   // const ipfs = new IPFS()
-  const file = `${offer}\n\n\n---\n\n\n${candidates}`
-  const hash = yield call(uploadFile, file)
+  // const file = `${offer}\n\n\n---\n\n\n${candidates}`
+  // const hash = yield call(uploadFile, file)
 
   // Notify UI about connection state
-  yield put(setWebRtcConnectionState(hash))
+  // yield put(setWebRtcConnectionState(hash))
 
   // Waiting for file hash from QR (UI)
   const { payload: qrAnswer } = yield take(setQrAnswer)
 
   // Request to download file by hash
-  const answer = yield call(downloadFile, qrAnswer)
+  // const answer = yield call(downloadFile, qrAnswer)
 
   // Push signed key to peers
-  yield call(rtc.pushAnswer, answer)
+  yield call(rtc.pushAnswer, qrAnswer /*answer*/)
   yield call(rtc.waitConnection)
 
   // cataChannel logic
