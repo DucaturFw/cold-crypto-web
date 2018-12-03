9d1cb2bae16de563f9fec595e02bc03eb4a1593f
zlumer <zlumer@gmail.com>
2018-12-03 15:45:52 +0300

    tests: fixture improvements & showQr()

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,37 +1,52 @@
 describe('login test', () =>
 {
+	function showQr(elemSelector: string, qrName: string)
+	{
+		cy.wait(800)
+		cy.document().then(doc =>
+		{
+			cy.fixture(`qr/${qrName}.mp4`, 'base64').then(mov =>
+			{
+				let uri = `data:video/mp4;base64,${mov}`
+				let el = doc.querySelector(elemSelector)
+
+				if (!el)
+					throw `video element "${elemSelector}" not found`
+		
+				el.setAttribute('src', uri)
+				cy.get('video').should('have.attr', 'src', uri)
+			})
+		})
+		cy.wait(600)
+	}
 	it('should render login page correctly', () =>
 	{
 		cy.visit('/')
 		cy.contains('Login using QR code').click()
 		cy.url().should('include', '/login')
 	})
+
+	it('should login with qr', () =>
+	{
+		cy.visit('/')
+		cy.contains('Login using QR code').click()
+
+		showQr('video', 'login_single_eth_wallet')
+
+		cy.url().should('include', '/wallets')
+		cy.contains('0x5DcD6E2D92bC4F96F9072A25CC8d4a3A4Ad07ba0')
+	})
 	
 	it.skip('should login with qr multiple wallets', () =>
 	{
 		cy.visit('/')
 		cy.contains('Login using QR code').click()
-		cy.wait(800) // TODO: hack, remove it
-	
-		cy.document().then(doc =>
-		{
-			cy.fixture('qr/login_qr_video.mp4', 'base64').then(mov =>
-			{
-				let uri = `data:video/mp4;base64,${mov}`
-				let el = doc.querySelector('video')
+		
+		showQr('video', 'login_multiple_eth_wallets')
 
-				if (!el)
-					throw "video element not found"
-		
-				el.setAttribute('src', uri)
-				cy.get('video').should('have.attr', 'src', uri)
-		
-				cy.wait(600)
-				cy.url().should('include', '/wallets')
-				cy.contains('0x5DcD6E2D92bC4F96F9072A25CC8d4a3A4Ad07ba0')
-				cy.contains('0x30384424F1Ab508F1f82b58f1335f343ABdF68AE')
-				cy.contains('0x1AD80eC32FD6Ef24e80801e90C5f7e32950C2D05')
-			})
-		})
+		cy.url().should('include', '/wallets')
+		cy.contains('0x5DcD6E2D92bC4F96F9072A25CC8d4a3A4Ad07ba0')
+		cy.contains('0x30384424F1Ab508F1f82b58f1335f343ABdF68AE')
+		cy.contains('0x1AD80eC32FD6Ef24e80801e90C5f7e32950C2D05')
 	})
 })
