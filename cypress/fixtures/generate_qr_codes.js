2a3054346152fde9848c6c06d13828034ee20c9f
zlumer <zlumer@gmail.com>
2018-12-03 16:13:59 +0300

    tests: fixture generator fix

--- cypress/fixtures/generate_qr_codes.js
+++ cypress/fixtures/generate_qr_codes.js
@@ -8,16 +8,17 @@ let qrs = require('./qrs.json')
 if (!fs.existsSync('qr'))
 	fs.mkdirSync('qr')
 
-del('qr/*')
-
-Object.keys(qrs).map(fileName =>
+del('qr/*').then(() =>
 {
-	// generate QR and save as image
-	let text = qrs[fileName]
-	return qr.toFile(`qr/${fileName}.png`, text).then(_ => fileName)
-}).map(p => p.then(fileName =>
-{
-	// generate video from QR image
-	videoshow([`qr/${fileName}.png`], { loop: 10, transition: false })
-		.save(`qr/${fileName}.mp4`)
-}))
+	Object.keys(qrs).map(fileName =>
+	{
+		// generate QR and save as image
+		let text = qrs[fileName]
+		return qr.toFile(`qr/${fileName}.png`, text).then(_ => fileName)
+	}).map(p => p.then(fileName =>
+	{
+		// generate video from QR image
+		videoshow([`qr/${fileName}.png`], { loop: 10, transition: false })
+			.save(`qr/${fileName}.mp4`)
+	}))
+})
