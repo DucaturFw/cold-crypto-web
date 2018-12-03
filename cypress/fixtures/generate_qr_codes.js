431a654d48333873d31044b7860832103e8f2451
zlumer <zlumer@gmail.com>
2018-12-03 15:45:52 +0300

    tests: fixture improvements & showQr()

--- cypress/fixtures/generate_qr_codes.js
+++ cypress/fixtures/generate_qr_codes.js
@@ -1,13 +1,14 @@
 const qr = require("qrcode")
 const videoshow = require('videoshow')
 const fs = require('fs')
+const del = require('del')
 
-let qrs = {
-	login_qr_video: `|2|[{"address":"0x5DcD6E2D92bC4F96F9072A25CC8d4a3A4Ad07ba0","blockchain":"eth","chainId":4},{"address":"0x30384424F1Ab508F1f82b58f1335f343ABdF68AE","blockchain":"eth","chainId":4},{"address":"0x1AD80eC32FD6Ef24e80801e90C5f7e32950C2D05","blockchain":"eth","chainId":4}]`,
-}
+let qrs = require('./qrs.json')
 
 if (!fs.existsSync('qr'))
 	fs.mkdirSync('qr')
+
+del('qr/*')
 
 Object.keys(qrs).map(fileName =>
 {
