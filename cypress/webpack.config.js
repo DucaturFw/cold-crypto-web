d8a2c95304f6c7ae4431c007aff9612800eff1b0
zlumer <zlumer@gmail.com>
2018-12-03 23:36:47 +0300

    tests: ignore typescript errors in webpack

--- cypress/webpack.config.js
+++ cypress/webpack.config.js
@@ -1,16 +1,19 @@
 module.exports = {
 	resolve: {
-	  extensions: ['.ts', '.js'],
+		extensions: ['.ts', '.js'],
 	},
 	module: {
-	  rules: [
-		{
-		  test: /\.ts$/,
-		  exclude: [/node_modules/],
-		  use: [{
-			loader: 'ts-loader',
-		  }],
-		},
-	  ],
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: [/node_modules/],
+				use: [{
+					loader: 'ts-loader',
+					options: {
+						happyPackMode: true
+					}
+				}],
+			},
+		],
 	},
-  }
+}
