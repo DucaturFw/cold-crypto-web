a603a0a80f270d6fc06db73949babdae4c15bc43
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-04 13:00:14 +0000

    Deploy DucaturFw/cold-crypto-web to github.com/DucaturFw/cold-crypto-web.git:gh-pages

---
+++ precache-manifest.f7a19f05ae8cd10f0b582319df94c598.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce22d80137d7fce13689",
+    "url": "./static/js/main.ce22d801.chunk.js"
+  },
+  {
+    "revision": "0c705d8cc5f3cd04af27",
+    "url": "./static/js/1.0c705d8c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4de6d2757107edc9f68890a44a5ac3ea",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cold/precache-manifest.685ed24de7852a6cffcf3e198a09cc62.js"
+  "./precache-manifest.f7a19f05ae8cd10f0b582319df94c598.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cold/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
