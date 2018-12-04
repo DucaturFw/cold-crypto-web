c00a69e24821c2f00c5f5fe69a380074a78addda
zlumer <zlumer@gmail.com>
2018-12-04 11:49:18 +0300

    action types & message saving

--- src/store/transport/reducer.ts
+++ src/store/transport/reducer.ts
@@ -1,14 +1,18 @@
 import { Reducer } from 'redux'
-import { ITransportState, TransportActionTypes } from './types'
+import { ITransportState, TransportActionTypes, TransportActionType } from './types'
 
 const initialState: ITransportState = {
   qrcodeData: '',
+  pushedMessages: [],
 }
 
-const reducer: Reducer<ITransportState> = (state = initialState, action) => {
+const reducer: Reducer<ITransportState, TransportActionType> = (state = initialState, action) => {
   switch (action.type) {
     case TransportActionTypes.SET_RTC_SID: {
       return { ...state, qrcodeData: action.payload }
+    }
+    case TransportActionTypes.SIGN_TX: {
+      return { ...state, pushedMessages: state.pushedMessages.concat(action.payload) }
     }
     default: {
       return state
--- src/store/transport/types.ts
+++ src/store/transport/types.ts
@@ -1,3 +1,5 @@
+import { IHostCommand } from "../../helpers/webrtc/hostproto"
+
 export enum TransportActionTypes {
   SEND_TX = '@@transport/SEND_TX',
   LOGIN = '@@transport/LOGIN',
@@ -6,7 +8,14 @@ export enum TransportActionTypes {
 
   SET_RTC_SID = '@@transport/SET_RTC_SID',
 }
+export type TransportActionType =
+  | { type: TransportActionTypes.SEND_TX }
+  | { type: TransportActionTypes.LOGIN }
+  | { type: TransportActionTypes.SIGN_TX, payload: IHostCommand<unknown[], unknown> }
+  | { type: TransportActionTypes.CREATE_TX }
+  | { type: TransportActionTypes.SET_RTC_SID, payload: string }
 
 export interface ITransportState {
   qrcodeData: string
+  pushedMessages: IHostCommand<unknown[], unknown>[]
 }
