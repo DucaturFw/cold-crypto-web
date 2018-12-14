d4d8f19ebf5db330a3f9bd94b9c339aff0b0ef1e
MaxSvargal <maxsvargal@gmail.com>
2018-12-14 21:32:32 +0700

    Restore to develop version

--- src/store/webrtc/connectSaga.ts
+++ src/store/webrtc/connectSaga.ts
@@ -1,10 +1,8 @@
-import { call, take, put, cancelled, select, race, fork } from 'redux-saga/effects'
-import { delay, eventChannel, Channel } from 'redux-saga'
-
-import { parseHostMessage } from '../../helpers/webrtc/hostproto'
+import { call, take, put, cancelled, select } from 'redux-saga/effects'
+import { eventChannel } from 'redux-saga'
 
 import { handshakeServerUrl } from '../../constants'
-import { connectionReady, sendCommand, setSender, incomingMessage } from './actions'
+import { connectionReady, sendCommand } from './actions'
 import { getWalletListCommand } from '../../helpers/jsonrps'
 import { setRtcSid } from '../transport/actions'
 import { IApplicationState } from '..'
@@ -16,9 +14,6 @@ const makeOfferRequest = (offer: string) =>
 const makeIceRequest = (ice: RTCIceCandidate) =>
   JSON.stringify({ jsonrpc: '2.0', id: 2, method: 'ice', params: { ice } })
 
-const makeRtcLoginRequest = (sid: string) =>
-  `webrtcLogin|1|${JSON.stringify({ sid, url: handshakeServerUrl })}`
-
 const onOpenChannel = (ws: WebSocket) =>
   eventChannel(emit => {
     ws.addEventListener('open', emit)
@@ -26,23 +21,11 @@ const onOpenChannel = (ws: WebSocket) =>
     return () => ws.removeEventListener('open', emit)
   })
 
-const onWsMessageChannel = (ws: WebSocket) =>
+const onMessageChannel = (ws: WebSocket) =>
   eventChannel(emit => {
     ws.addEventListener('message', emit)
     return () => ws.removeEventListener('message', emit)
-  }) as Channel<MessageEvent>
-
-const onRtcConnectedChannel = (rtc: RTCHelper) =>
-  eventChannel(emit => {
-    rtc.on('connected', err => emit(err))
-    return () => false
   })
-
-const onRtcMessageChannel = (rtc: RTCHelper) =>
-  eventChannel(emit => {
-    rtc.on('msg', err => emit(err))
-    return () => false
-  }) as Channel<MessageEvent>
 
 function* answerSaga(ws: WebSocket, rtc: RTCHelper, answer: string) {
   const sendIce = (ice: RTCIceCandidate) => ws.send(makeIceRequest(ice)) // TODO: Add typings
@@ -54,115 +37,39 @@ function* answerSaga(ws: WebSocket, rtc: RTCHelper, answer: string) {
   return ws.close()
 }
 
-function* timeoutOnWsAnswer(wsMessageChan: Channel<MessageEvent>) {
-  while (true) {
-    const { data } = yield take(wsMessageChan)
-    const { method } = JSON.parse(data.toString())
-    if (method === 'answer')
-      return yield delay(7000)
-  }
-}
-
-function* wrapRtcMessages(rtcMessageChan: Channel<MessageEvent>) {
-  while (true) {
-    const { data } = yield take(rtcMessageChan)
-    console.log('rtc recieved', data)
-    yield put(incomingMessage(parseHostMessage(data))) // TODO: Check format? Add typings!
-  }
-}
-
-function* proxyIncomingFallbackToRtc(wsMessageChan: Channel<MessageEvent>) {
-  while (true) {
-    const { data } = yield take(wsMessageChan)
-    console.log('ws recieved', data)
-    yield put(incomingMessage({
-      method: 'fallback',
-      params: {
-        msg: parseHostMessage(data)
-      }
-    }))
-  }
-}
-
-function* proxyFallbackMessagesToWs(rtcMessageChan: Channel<MessageEvent>, ws: WebSocket) {
-  while (true) {
-    const { payload } = yield take(rtcMessageChan)
-    const msg = JSON.stringify({
-      jsonrpc: '2.0',
-      id: 789,
-      method: 'fallback',
-      params: { msg: payload }
-    })
-
-    ws.send(msg)
-    console.log('sended', msg)
-  }
-}
-
-function* watchForIncomingMessagesSaga(rtc: RTCHelper, ws: WebSocket) {
-  while (true) try {
-    const { payload } = yield take(incomingMessage)
-    const data = payload.method && payload.method === 'fallback' ? payload.params.msg : payload
-    const { id, method, result, params } = data
-
-    if (id === 1)
-      yield put(setRtcSid(makeRtcLoginRequest(result.sid)))
-
-    if (method === 'ice')
-      yield call(rtc.pushIceCandidate, params.ice)
-
-    if (method === 'answer')
-      return yield call(answerSaga, ws, rtc, params.answer)
-
-  } catch (err) {
-    console.log(err)
-    return err
-  } finally {
-    if (yield cancelled()) {
-      yield put(sendCommand(getWalletListCommand()))
-    }
-  }
-}
-
 export default function* connectSaga() {
-  // TODO: eject webrtc instance from redux store, pass it directly
   const rtc = yield select((state: IApplicationState) => state.webrtc.rtc)
   const offerPromise = yield call(rtc.createOffer)
   const ws = new WebSocket(handshakeServerUrl)
   const openChan = onOpenChannel(ws)
-
-  // Waiting for WS, we can't work without this critical connection
-  // TODO: change flow to offline qr codes?
+  const messageChan = onMessageChannel(ws)
   yield take(openChan)
 
   ws.send(makeOfferRequest(offerPromise.sdp))
 
-  // const rtcConnectedChan = onRtcConnectedChannel(rtc)
-  const rtcMessageChan = onRtcMessageChannel(rtc)
-  const wsMessageChan = onWsMessageChannel(ws)
+  while (true)
+    try {
+      const { data } = yield take(messageChan)
+      const { id, method, result, params } = JSON.parse(data.toString())
 
-  // const [ rtcConnected ] = yield race([
-  //   take(rtcConnectedChan),
-  //   delay(5000),
-  //   call(timeoutOnWsAnswer, wsMessageChan)
-  // ] as any /* TODO: update types of redux-saga */)
+      if (id === 1) yield put(setRtcSid(webrtcLogin(result.sid)))
+      if (method === 'ice') yield call(rtc.pushIceCandidate, params.ice)
+      if (method === 'answer')
+        return yield call(answerSaga, ws, rtc, params.answer)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      if (yield cancelled()) {
+        openChan.close()
+        messageChan.close()
+        console.log('ws connection closed')
+        yield put(sendCommand(getWalletListCommand()))
+      }
+    }
+}
 
-  yield fork(proxyFallbackMessagesToWs, rtcMessageChan, ws)
-  yield fork(proxyIncomingFallbackToRtc, wsMessageChan)
-  yield fork(wrapRtcMessages, rtcMessageChan)
+export const webrtcLogin = (sid: string) => {
+  const params = { sid, url: handshakeServerUrl }
 
-  // if (!rtcConnected) {
-  //   yield fork(proxyFallbackMessagesToWs, rtcMessageChan, ws)
-  //   yield fork(proxyIncomingFallbackToRtc, wsMessageChan)
-  // }
-
-  // yield fork(watchForWsClose(ws)) TODO: implement for catch future disconnections
-
-  // Waiting for end of messages loop
-  yield call(watchForIncomingMessagesSaga, rtc, ws)
-
-  openChan.close()
-  wsMessageChan.close()
-  // TODO: close over channels
-  console.log('ws connection closed')
+  return `webrtcLogin|1|${JSON.stringify(params)}`
 }
